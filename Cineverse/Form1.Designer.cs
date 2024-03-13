58cbd6d597f6ad27c4f085c022abcf0231b562c5
Iner <inercollado52@gmail.com>
2024-03-13 13:35:27 +0800

    test

--- Cineverse/Form1.Designer.cs
+++ Cineverse/Form1.Designer.cs
@@ -34,9 +34,15 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(300, 92);
+            this.label1.Location = new System.Drawing.Point(227, 180);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size;
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(729, 631);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -46,6 +52,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
