7facb515fc2961b7beaf1745f953475f1b892008
Iner <inercollado52@gmail.com>
2024-03-13 13:37:59 +0800

    test

--- Cineverse/Form1.Designer.cs
+++ Cineverse/Form1.Designer.cs
@@ -27,9 +27,25 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
-        {            this.components = new System.ComponentModel.Container();
+        {            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(382, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -39,6 +55,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
