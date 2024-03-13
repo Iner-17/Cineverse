83394e27fbf62154858d43b3048ae6a377f01258
Iner <inercollado52@gmail.com>
2024-03-13 12:51:34 +0800

    Add project files.

---
+++ Cineverse/DBConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+namespace Cineverse
+{
+    internal class DBConnection
+    {
+
+        private static readonly string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=cineversedb;";
+
+        public static MySqlConnection getConnection()
+        {
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            return conn;
+        }
+    }
+}
