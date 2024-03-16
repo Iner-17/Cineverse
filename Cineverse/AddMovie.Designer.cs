4ce63dabfa0f6f1bee1c970275009237500ed19a
Iner <inercollado52@gmail.com>
2024-03-16 14:12:19 +0800

    wer

--- Cineverse/AddMovie.Designer.cs
+++ Cineverse/AddMovie.Designer.cs
@@ -71,7 +71,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(73, 109);
+            this.label2.Location = new System.Drawing.Point(59, 109);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(99, 19);
             this.label2.TabIndex = 6;
@@ -83,7 +83,7 @@
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox1.Font = new System.Drawing.Font("Montserrat", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.ForeColor = System.Drawing.Color.White;
-            this.textBox1.Location = new System.Drawing.Point(76, 138);
+            this.textBox1.Location = new System.Drawing.Point(62, 138);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(270, 31);
             this.textBox1.TabIndex = 7;
@@ -94,7 +94,7 @@
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox2.Font = new System.Drawing.Font("Montserrat", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox2.ForeColor = System.Drawing.Color.White;
-            this.textBox2.Location = new System.Drawing.Point(401, 138);
+            this.textBox2.Location = new System.Drawing.Point(387, 138);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 31);
             this.textBox2.TabIndex = 9;
@@ -105,7 +105,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(397, 109);
+            this.label3.Location = new System.Drawing.Point(383, 109);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(51, 19);
             this.label3.TabIndex = 8;
@@ -117,7 +117,7 @@
             this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox3.Font = new System.Drawing.Font("Montserrat", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox3.ForeColor = System.Drawing.Color.White;
-            this.textBox3.Location = new System.Drawing.Point(531, 138);
+            this.textBox3.Location = new System.Drawing.Point(517, 138);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(193, 31);
             this.textBox3.TabIndex = 11;
@@ -127,7 +127,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(527, 109);
+            this.label4.Location = new System.Drawing.Point(513, 109);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(58, 19);
             this.label4.TabIndex = 10;
@@ -139,7 +139,7 @@
             this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox4.Font = new System.Drawing.Font("Montserrat", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox4.ForeColor = System.Drawing.Color.White;
-            this.textBox4.Location = new System.Drawing.Point(753, 138);
+            this.textBox4.Location = new System.Drawing.Point(739, 138);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(145, 31);
             this.textBox4.TabIndex = 13;
@@ -149,7 +149,7 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(749, 109);
+            this.label5.Location = new System.Drawing.Point(735, 109);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(140, 19);
             this.label5.TabIndex = 12;
@@ -160,7 +160,7 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.White;
-            this.label6.Location = new System.Drawing.Point(75, 211);
+            this.label6.Location = new System.Drawing.Point(61, 211);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(182, 19);
             this.label6.TabIndex = 15;
@@ -171,7 +171,7 @@
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.White;
-            this.label7.Location = new System.Drawing.Point(440, 211);
+            this.label7.Location = new System.Drawing.Point(426, 211);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(184, 19);
             this.label7.TabIndex = 16;
@@ -184,7 +184,7 @@
             this.btn_add1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_add1.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_add1.ForeColor = System.Drawing.Color.Transparent;
-            this.btn_add1.Location = new System.Drawing.Point(273, 244);
+            this.btn_add1.Location = new System.Drawing.Point(259, 244);
             this.btn_add1.Name = "btn_add1";
             this.btn_add1.Size = new System.Drawing.Size(73, 27);
             this.btn_add1.TabIndex = 19;
@@ -198,7 +198,7 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.Transparent;
-            this.button1.Location = new System.Drawing.Point(608, 245);
+            this.button1.Location = new System.Drawing.Point(594, 245);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(73, 27);
             this.button1.TabIndex = 20;
@@ -212,7 +212,7 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Montserrat", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(19)))), ((int)(((byte)(19)))));
-            this.button2.Location = new System.Drawing.Point(722, 525);
+            this.button2.Location = new System.Drawing.Point(708, 525);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(196, 35);
             this.button2.TabIndex = 22;
@@ -224,7 +224,7 @@
             this.comboBox1.BackColor = System.Drawing.SystemColors.Window;
             this.comboBox1.Font = new System.Drawing.Font("Montserrat", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(77, 353);
+            this.comboBox1.Location = new System.Drawing.Point(63, 353);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(269, 27);
             this.comboBox1.TabIndex = 23;
@@ -233,7 +233,7 @@
             // 
             this.comboBox2.Font = new System.Drawing.Font("Montserrat", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(444, 353);
+            this.comboBox2.Location = new System.Drawing.Point(430, 353);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(237, 27);
             this.comboBox2.TabIndex = 24;
@@ -243,7 +243,7 @@
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.Color.White;
-            this.label8.Location = new System.Drawing.Point(76, 321);
+            this.label8.Location = new System.Drawing.Point(62, 321);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(107, 19);
             this.label8.TabIndex = 25;
@@ -254,7 +254,7 @@
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.ForeColor = System.Drawing.Color.White;
-            this.label9.Location = new System.Drawing.Point(440, 321);
+            this.label9.Location = new System.Drawing.Point(426, 321);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(109, 19);
             this.label9.TabIndex = 26;
@@ -268,7 +268,7 @@
             this.label10.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label10.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label10.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.label10.Location = new System.Drawing.Point(366, 142);
+            this.label10.Location = new System.Drawing.Point(352, 142);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(33, 25);
             this.label10.TabIndex = 28;
@@ -278,7 +278,7 @@
             // pictureBox1
             // 
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(722, 211);
+            this.pictureBox1.Location = new System.Drawing.Point(708, 211);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(196, 296);
             this.pictureBox1.TabIndex = 21;
@@ -312,7 +312,7 @@
             this.dateTimePicker2.CustomFormat = "HH:mm";
             this.dateTimePicker2.Font = new System.Drawing.Font("Montserrat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker2.Location = new System.Drawing.Point(443, 243);
+            this.dateTimePicker2.Location = new System.Drawing.Point(429, 243);
             this.dateTimePicker2.MaximumSize = new System.Drawing.Size(150, 30);
             this.dateTimePicker2.MinimumSize = new System.Drawing.Size(150, 30);
             this.dateTimePicker2.Name = "dateTimePicker2";
@@ -330,7 +330,7 @@
             this.dateTimePicker1.CustomFormat = "MMM dd, yyy";
             this.dateTimePicker1.Font = new System.Drawing.Font("Montserrat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker1.Location = new System.Drawing.Point(77, 242);
+            this.dateTimePicker1.Location = new System.Drawing.Point(63, 242);
             this.dateTimePicker1.MaximumSize = new System.Drawing.Size(180, 30);
             this.dateTimePicker1.MinimumSize = new System.Drawing.Size(180, 30);
             this.dateTimePicker1.Name = "dateTimePicker1";
