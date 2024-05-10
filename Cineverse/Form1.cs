853de6660b58af7a20d6c4695d2f9a71f6632f61
Bianca-217 <sweetkatrinabiancaignacio@gmail.com>
2024-05-10 08:52:25 +0800

    enter

--- Cineverse/Form1.cs
+++ Cineverse/Form1.cs
@@ -205,6 +205,7 @@ namespace Cineverse
             {
                 txt_user.BorderColor = SystemColors.ControlDark;
                 ep_username.SetError(txt_user, string.Empty);
+
             }
         }
 
