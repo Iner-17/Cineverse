baa32123e822bbea1b42bc19ebadf4e862613306
Iner <inercollado52@gmail.com>
2024-05-21 16:53:33 +0800

    fix admin profile not getting data

--- Cineverse/UserControls/UserSection.cs
+++ Cineverse/UserControls/UserSection.cs
@@ -28,6 +28,7 @@ namespace Cineverse.UserControls
                 btn_timeOut.Visible= false;
 
                 lbl_myProfile.Text = "ADMIN'S PROFILE";
+                getAdminInfo();
             } 
 
 
@@ -35,19 +36,19 @@ namespace Cineverse.UserControls
             string formattedDate = currentDate.ToString("dddd, MMMMM d, yyyy");
 
             lbl_date.Text = formattedDate;
-            getUserInfo();
+            getCashierInfo();
         }
 
         int userId = 0;
 
-        private void getUserInfo()
+        private void getCashierInfo()
         {
             MySqlConnection conn = DBConnection.getConnection();
 
             try
             {
                 conn.Open();
-                string query = "SELECT concat(firstname, ' ' , lastname) as name, email, password, phone_number, time_in FROM accounts INNER JOIN employee ON accounts.user_id = employee.user_id where accounts.username = @Username";
+                string query = "SELECT concat(firstname, ' ' , lastname) as name, email, password, phone_number, time_in FROM accounts INNER JOIN employee ON accounts.user_id = employee.user_id WHERE accounts.username = @Username";
                 MySqlCommand cmd = new MySqlCommand(query, conn);
 
                 cmd.Parameters.AddWithValue("Username", Login.Username);
@@ -68,12 +69,49 @@ namespace Cineverse.UserControls
                     lbl_pass.Text = decryptedPass.Replace(decryptedPass, counter);
                     lbl_contactNo.Text = reader["phone_number"].ToString();
                     lbl_timein.Text = reader["time_in"].ToString();
-                    
+
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+            finally { conn.Close(); }
+
+        }
+
+
+        private void getAdminInfo()
+        {
+            MySqlConnection conn = DBConnection.getConnection();
+
+            try
+            {
+                conn.Open();
+                string query = "SELECT concat(firstname, ' ' , lastname) as name, email, password, phone_number FROM accounts WHERE accounts.username = @Username";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("Username", Login.Username);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    string pass = reader["password"].ToString();
+                    string counter = "";
+                    string decryptedPass = Security.Decrypt(pass);
+
+                    for (int i = 0; i < decryptedPass.Length; i++)
+                    {
+                        counter += '*';
+                    }
+
+                    lbl_name.Text = reader["name"].ToString();
+                    lbl_email.Text = reader["email"].ToString();
+                    lbl_pass.Text = decryptedPass.Replace(decryptedPass, counter);
+                    lbl_contactNo.Text = reader["phone_number"].ToString();
+
                 }
             }
             catch (Exception ex) { MessageBox.Show(ex.Message); }
             finally { conn.Close(); }
         }
+
         private string fileName = "";
         private void btn_edit_Click(object sender, EventArgs e)
         {
